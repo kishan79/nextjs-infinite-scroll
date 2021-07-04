3f08a7c8636a7a3476cc038119ad078956ab1c18
Kishan <kishan.237@gmail.com>
2021-07-04 23:55:05 +0530

    Code refactor

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../../styles/Home.module.css";
 
 const siteTitle = 'Nextjs Infinite Scroll';
@@ -17,19 +16,6 @@ export default function Layout({children}) {
       <main className={styles.main}>
           {children}
       </main>
-
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer> */}
     </div>
   );
 }
